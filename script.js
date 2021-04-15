e4362532a54a46fc623d9d918799b32d7b469855
Duncan Jacobsen <jacobsenduncan@gmail.com>
2021-04-14 19:17:05 -0600

    added error/loading states. cleaned things up

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-
-// const tagged = document.getElementById('queryImput').value
-
-function handleSubmit(){
+// check user submission, call api
+function handleSubmit() {
 
     // grab user input
     const tagged = document.getElementById('userQuery').value;
-    
+
     // ensure that user has entered a search query before making api call
-    if(tagged){
+    if (tagged) {
+        // inform user that the data is being grabbed
+        informUser('Loading...')
+
         getData(tagged)
     }
-    else{
+    else {
         // inform user that a search query must be entered
-        console.log('please enter a search query')
+        informUser('Please enter a search query')
     }
 
     // prevent page from refreshing on submit
     return false
 }
 
-function getData(query){
+// get data from the stack overflow api
+function getData(query) {
     // use proxy to get around CORS issues
     // proxy source: https://github.com/Rob--W/cors-anywhere.git
     const proxy = 'https://nameless-journey-40666.herokuapp.com/'
     const url = `https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&tagged=${query}&site=stackoverflow`
-    
+
     const request = proxy + url;
 
     // call API
@@ -32,42 +34,50 @@ function getData(query){
         method: 'GET',
         mode: 'cors',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         }
     })
-      .then(response => response.json())
-      .then(data => {
-          if(data.items.length === 0){
-              console.log('No items matched your search query. Try something like \'reactjs\' or \'python\'!')
-          }
-          else{
-              console.log(data)
-              generateTable(data.items)
-          }
-      })
+        .then(response => response.json())
+        .then(data => {
+            if (data.items.length === 0) {
+                informUser('No items matched your search query. Try something like \'reactjs\' or \'python\'!')
+            }
+            else {
+                generateTable(data.items)
+            }
+        })
+}
+
+// upon successful api call, organize data into table rows
+function generateTable(items) {
+    // generate table data
+    let tableData = ''
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i]
+
+        // convert unix time to date time
+        let creationDate = new Date(0)
+        creationDate.setUTCSeconds(item.creation_date)
+
+        // add data to table in a row
+        tableData += `
+        <tr>
+            <td>${item.owner.display_name}</td>
+            <td><a href='${item.link}' target="_blank">${item.title}</a></td>
+            <td>${creationDate.getMonth()}/${creationDate.getDate()}/${creationDate.getFullYear()}</td>
+        </tr>`
     }
 
-    function generateTable(items){
-        // generate table data
-        let tableData = ''
-        for(let i = 0; i < items.length; i++){
-            let item = items[i]
+    // append data to table
+    document.getElementById('tableBody').innerHTML = tableData;
 
-            // convert unix time to date time
-            let creationDate = new Date(0)
-            creationDate.setUTCSeconds(item.creation_date)
+    // remove user message, show table
+    informUser('')
+    document.getElementById('questionTable').style.display = 'table'
+    
+}
 
-            // add data to table in a row
-            tableData += `
-                <tr>
-                    <td>${item.owner.display_name}</td>
-                    <td><a href='${item.link}' target="_blank">${item.title}</a></td>
-                    <td>${creationDate.getMonth()}/${creationDate.getDate()}/${creationDate.getFullYear()}</td>
-                </tr>`
-        }
-
-        // append data to table
-        document.getElementById('tableBody').innerHTML = tableData;
-    }
-
-    getData('php')
+// function to update message displayed to user
+function informUser(message){
+    document.getElementById('userMessage').textContent = message
+}
